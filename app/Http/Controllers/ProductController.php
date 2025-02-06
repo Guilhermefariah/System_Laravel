11f306b45ca8a4f1c5ab71cc78a65a63c8ebb5fd
Guilhermefariah <Guilhermefaria321@gmail.com>
2025-02-06 07:56:43 -0300

    Implementando o método index com view

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,7 +13,7 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::all();
-        return view('index');
+        return view('index', ['products' => $products]);
     }
 
     /**
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,30 @@
-@extends('app')
+@extends('templates.app')
 
 @section('content')
     <div class="container">
-        <h1>Produtos</h1>
         <p>Lista de produtos</p>
+
+        <table class="table table-condensed">
+            <thead>
+                <tr>
+                    <td>ID</td>
+                    <td>Nome</td>
+                    <td>Preço</td>
+                    <td>Descrição</td>
+                    <td>Ações</td>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($products as $product)
+                    <tr>
+                        <td>{{ $product->id }}</td>
+                        <td>{{ $product->name }}</td>
+                        <td>{{ $product->price }}</td>
+                        <td>{{ $product->description }}</td>
+                        <td></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
