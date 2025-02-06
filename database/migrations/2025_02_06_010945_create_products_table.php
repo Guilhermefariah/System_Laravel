901c30e26a5cf35647d3c4fa2c111e3ae5cf7a93
Guilhermefariah <Guilhermefaria321@gmail.com>
2025-02-05 22:13:15 -0300

    Colunas migration

--- database/migrations/2025_02_06_010945_create_products_table.php
+++ database/migrations/2025_02_06_010945_create_products_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->double('price', 6, 2);
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
