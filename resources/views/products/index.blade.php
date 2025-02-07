151417506a9b68c0749782f746159d1b553fe689
Guilhermefariah <Guilhermefaria321@gmail.com>
2025-02-07 10:57:53 -0300

    adicionando links para as rotas

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,6 +4,8 @@
     <div class="container">
         <p>Lista de produtos</p>
 
+        <p><a href="{{ route('products.create') }}" class="btn btn-success" >Novo</a></p>
+
         <table class="table table-condensed">
             <thead>
                 <tr>
@@ -18,10 +20,11 @@
                 @foreach ($products as $product)
                     <tr>
                         <td>{{ $product->id }}</td>
-                        <td>{{ $product->name }}</td>
+                        <td><a href="{{ route('products.show', $product->id) }}">{{ $product->name }}</a></td>
                         <td>{{ $product->price }}</td>
                         <td>{{ $product->description }}</td>
                         <td>
+                            <a href="{{ route('products.edit', $product->id) }}" class="btn btn-primary">Editar</a>
                             {{ Form::open(array('route' => array('products.destroy', $product->id), 'method' => 'delete')) }}
                             {{ Form::submit('Excluir', ['class' => 'btn btn-danger']) }}
                             {{ Form::close() }}
