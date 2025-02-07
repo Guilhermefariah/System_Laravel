2781f1208418561f01fb0a75cf4ba4ed6d4aa4c3
Guilhermefariah <Guilhermefaria321@gmail.com>
2025-02-07 13:47:49 -0300

    alterando a rota welcome para index

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('products.index');
 });
 
+Route::get('/', [ProductController::class, 'index'])->name('products.index');
+
 Route::group(['prefix' => 'products'], function() {
-    Route::get('/', [ProductController::class, 'index'])->name('products.index');
     Route::get('create', [ProductController::class, 'create'])->name('products.create');
     Route::post('/', [ProductController::class, 'store'])->name('products.store');
     Route::get('{id}', [ProductController::class, 'show'])->name('products.show');
